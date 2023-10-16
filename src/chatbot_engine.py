6d8bc83629a1c40c31f6c3e4111464a9e4315f20
t-yasui <yasutai12@gmail.com>
2023-10-16 22:45:49 +0900

    completed chat app

---
+++ src/chatbot_engine.py
@@ -0,0 +1,15 @@
+from langchain.chat_models import ChatOpenAI
+import langchain
+from langchain.memory import ChatMessageHistory
+from langchain.schema import HumanMessage
+
+
+langchain.verbose = True
+
+def chat(message: str, history: ChatMessageHistory) -> str:
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+    messages = history.messages
+    messages.append(HumanMessage(content=message))
+
+    return llm(messages).content
--- src/gradio_app.py
+++ src/gradio_app.py
@@ -1,8 +1,27 @@
 import gradio as gr
+from dotenv import load_dotenv
+from chatbot_engine import chat
+from langchain.memory import ChatMessageHistory
 
-def greet(name):
-    return "Hello " + name + "!"
+def respond(message, chat_history):
+    history = ChatMessageHistory()
+    for [user_message, ai_message] in chat_history:
+        history.add_user_message(user_message)
+        history.add_user_message(ai_message)
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-    
-demo.launch()   
+    bot_message = chat(message, history)
+    chat_history.append((message, bot_message))
+    return "", chat_history
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("Clear")
+
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+    clear.click(lambda: None, None, chatbot, queue=False)
+
+if __name__ == "__main__":
+    load_dotenv()
+    demo.launch()
