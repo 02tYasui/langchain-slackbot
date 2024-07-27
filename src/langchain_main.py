f5274492cf321a32f1ef58b589d3be45f21502d3
t-yasui <yasutai12@gmail.com>
2024-07-28 00:01:29 +0900

    会話履歴をDynamoDBに保存するロジック追加

--- src/langchain_main.py
+++ src/langchain_main.py
@@ -1,20 +1,15 @@
+import os
+import boto3 #Lambdaで動作する際に必要
 from langchain_openai import ChatOpenAI
-from langchain_core.chat_history import (
-    BaseChatMessageHistory,
-    InMemoryChatMessageHistory,
-)
+from langchain_community.chat_message_histories import DynamoDBChatMessageHistory
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
-MODEL_NAME = "gpt-4o-mini"
-
-store = {}
-
 
 def chat_with_history():
     """履歴保持チャット"""
-    model = ChatOpenAI(model=MODEL_NAME, temperature=0)
+    model = ChatOpenAI(model=os.environ.get("MODEL_NAME"), temperature=0)
 
     prompt = ChatPromptTemplate.from_messages(
         [
@@ -28,16 +23,11 @@ def chat_with_history():
 
     runnable_agent = RunnableWithMessageHistory(
         runnable=chain,
-        get_session_history=_get_session_history,
+        get_session_history=lambda session_id: DynamoDBChatMessageHistory(
+            table_name=os.environ.get("DYNAMODB_TABLE_NAME"), session_id=session_id
+        ),
         input_messages_key="user_input",
         history_messages_key="history",
     )
 
     return runnable_agent
-
-
-def _get_session_history(session_id: str) -> BaseChatMessageHistory:
-    """ローカルメモリにセッション履歴保存・取得"""
-    if session_id not in store:
-        store[session_id] = InMemoryChatMessageHistory()
-    return store[session_id]
